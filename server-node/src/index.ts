ffbfdc84ce6671592f09c3b3b0ad8b75d83eb97f
Pallishree <pallishreebehera01@gmail.com>
2025-07-12 13:19:16 +0530

    docker file added

--- server-node/src/index.ts
+++ server-node/src/index.ts
@@ -7,6 +7,7 @@ import FormData from "form-data";
 import axios from "axios";
 import { promisify } from "util";
 import { pipeline } from "stream";
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
